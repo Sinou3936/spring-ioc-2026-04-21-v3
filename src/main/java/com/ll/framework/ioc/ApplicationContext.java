4e827d8aef98a83e2a85d6d28ee62b8455359ada
Sinou3936 <usa3707@naver.com>
2026-04-27 17:31:19 +0900

    t7~t8 GREEN

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,9 +1,11 @@
 package com.ll.framework.ioc;
 
 import com.ll.framework.ioc.annotations.*;
+import lombok.SneakyThrows;
 import org.reflections.Reflections;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class ApplicationContext {
@@ -11,6 +13,8 @@ public class ApplicationContext {
     private final String basePackage;
     private final Map<String, Object> beans = new HashMap<>();
     private final List<Class<?>> classes = new ArrayList<>();
+    private final Map<String, Method> beanMethods = new HashMap<>();
+    private final Map<String,Object> configInstance = new HashMap<>();
 
     public ApplicationContext(String basePackage) {
         this.basePackage = basePackage;
@@ -27,12 +31,32 @@ public class ApplicationContext {
         beanClasses.addAll(reflections.getTypesAnnotatedWith(Bean.class));
 
         classes.addAll(beanClasses);
+
+        for(Class<?> configClass : reflections.getTypesAnnotatedWith(Configuration.class))
+        {
+            for(Method method : configClass.getDeclaredMethods())
+            {
+                if(method.isAnnotationPresent(Bean.class))
+                    beanMethods.put(method.getName(),method);
+            }
+        }
     }
 
+    @SneakyThrows
     public <T> T genBean(String beanName) {
         if(beans.containsKey(beanName))
         {
             return (T) beans.get(beanName);
+        }
+        else if(beanMethods.containsKey(beanName))
+        {
+            Method method = beanMethods.get(beanName);
+            Object objectInstance = getOrCreateConfigInstance(method.getDeclaringClass());
+
+            Object[] args = resolverArgs(method);
+            Object bean = method.invoke(objectInstance, args);
+            beans.put(beanName, bean);
+            return (T) bean;
         }
 
         for(Class<?> clazz: classes)
@@ -47,6 +71,37 @@ public class ApplicationContext {
         }
 
         return null;
+    }
+
+    private Object[] resolverArgs(Method method) {
+        try{
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            Object[] args = new Object[parameterTypes.length];
+
+            for(int i = 0; i<args.length; i++){
+                args[i] = getBeanByType(parameterTypes[i]);
+            }
+            return args;
+        }catch (Exception e){
+            throw new RuntimeException("Failed to create : "+ method.getName(),e);
+        }
+
+    }
+
+    private Object getOrCreateConfigInstance(Class<?> declaringClass) {
+        String className = declaringClass.getName();
+        if(configInstance.containsKey(className))
+        {
+            return configInstance.get(className);
+        }
+
+        try{
+            Object instance = declaringClass.getDeclaredConstructor().newInstance();
+            configInstance.put(className,instance);
+            return instance;
+        }catch (Exception e){
+            throw new RuntimeException("Failed to create config instance : "+ declaringClass.getName(), e);
+        }
     }
 
     private Object createInstance(Class<?> clazz) {
@@ -78,7 +133,6 @@ public class ApplicationContext {
                 return genBean(beanName);
             }
         }
-
         return null;
     }
 
